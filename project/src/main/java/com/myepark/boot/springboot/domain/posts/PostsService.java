2ec27bd0af49314c8d75b6634c2fd5448b8b63f0
Azderica <mh97888@gmail.com>
2020-09-30 15:02:28 +0900

    add some crud api

--- project/src/main/java/com/myepark/boot/springboot/domain/posts/PostsService.java
+++ project/src/main/java/com/myepark/boot/springboot/domain/posts/PostsService.java
@@ -1,5 +1,7 @@
 package com.myepark.boot.springboot.domain.posts;
 
+import com.myepark.boot.springboot.web.dto.PostsResponseDto;
+import com.myepark.boot.springboot.web.dto.PostsUpdateRequestDto;
 import com.myepark.boot.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
---
+++ project/src/main/java/com/myepark/boot/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.myepark.boot.springboot.web.dto;
+
+import com.myepark.boot.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
