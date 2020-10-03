c89055e2ceac6447cc46ba5b9d7b1ae22bd01151
Azderica <mh97888@gmail.com>
2020-10-03 11:17:55 +0900

    update index page

--- project/src/main/java/com/myepark/boot/springboot/web/IndexController.java
+++ project/src/main/java/com/myepark/boot/springboot/web/IndexController.java
@@ -1,18 +1,28 @@
 package com.myepark.boot.springboot.web;
 
+import com.myepark.boot.springboot.domain.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+//
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
