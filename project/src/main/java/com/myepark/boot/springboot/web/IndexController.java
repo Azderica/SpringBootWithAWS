109d9cabc46e2884e4124b36618f4b1ce148780f
Azderica <mh97888@gmail.com>
2020-10-03 11:34:15 +0900

    create update function

--- project/src/main/java/com/myepark/boot/springboot/web/IndexController.java
+++ project/src/main/java/com/myepark/boot/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.myepark.boot.springboot.web;
 
 import com.myepark.boot.springboot.domain.posts.PostsService;
+import com.myepark.boot.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,14 +20,18 @@ public class IndexController {
         return "index";
     }
 
-//    @GetMapping("/")
-//    public String index() {
-//        return "index";
-//    }
-//
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+
+        return "posts-update";
     }
 
 }
