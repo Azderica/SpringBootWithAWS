43e42683ca677f9eb9f39614f477ff37cc032dcd
Azderica <mh97888@gmail.com>
2020-10-09 14:54:18 +0900

    fix all error

--- project/src/test/java/com/myepark/boot/springboot/web/PostsApiControllerTest.java
+++ project/src/test/java/com/myepark/boot/springboot/web/PostsApiControllerTest.java
@@ -13,15 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -30,6 +25,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -75,6 +71,7 @@ public class PostsApiControllerTest {
                 .content(content)
                 .author("author")
                 .build();
+
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         // when
@@ -90,15 +87,16 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    @WithMockUser(roles = "USER")
-    public  void Posts_수정된다() throws Exception {
-        // given
+    @WithMockUser(roles="USER")
+    public void Posts_수정된다() throws Exception {
+        //given
         Posts savedPosts = postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
                 .author("author")
                 .build());
-        Long updatedId = savedPosts.getId();
+
+        Long updateId = savedPosts.getId();
         String expectedTitle = "title2";
         String expectedContent = "content2";
 
@@ -107,11 +105,11 @@ public class PostsApiControllerTest {
                 .content(expectedContent)
                 .build();
 
-        String url = "http://localhost:" + port + "/api/v1/posts/" + updatedId;
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
         // when
-        mvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(new ObjectMapper().writeValueAsString(requestDto)))
                 .andExpect(status().isOk());
 
